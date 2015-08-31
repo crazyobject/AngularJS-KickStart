186c368209a89567e463a7b68a50a572a7345b93
dhanesh <dhanesh.mane@xoriant.com>
2015-08-31 20:06:16 +0530

    https issue with stock service

--- app/stock/stockService.js
+++ app/stock/stockService.js
@@ -8,7 +8,7 @@ angular.module('myApp.stock.stockService', [])
 	API.getStockPrice = function(){
 		return $http({
 			method: 'GET', 
-			url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%20%28%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22%29&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json'
+			url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%20%28%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22%29&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json'
 		  });
 	}
 	
